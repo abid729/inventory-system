ad6c016cb1e6a12342b2fab3d4a2b73f34ba8a20
abid729 <abidali31570@gmail.com>
2023-01-05 12:09:59 +0500

    first commit

---
+++ pilar-of-opp.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+class Student{
+private:
+string name;
+int age;
+char reference-no;
+
+public:
+int getAge(){
+    return this->age;
+}
+};
+
+int main(){
+    Student first;
+    cout<< "Every thing is working Perfect";
+    return 0;
+}
